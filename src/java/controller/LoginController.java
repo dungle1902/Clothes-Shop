df7029277d74c8bdd26fae1f05b5fa518fb82954
le tien dung <dunglthe182158@fpt.edu.vn>
2024-11-24 16:20:04 +0700

    project web for prj301

---
+++ src/java/controller/LoginController.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import dal.AcountDBContext;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import model.Account;
+import org.apache.tomcat.util.codec.binary.Base64;
+
+@WebServlet(name = "LoginController", urlPatterns = {"/login"})
+public class LoginController extends HttpServlet {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String user = request.getParameter("Username");
+        String pass = request.getParameter("Password");
+        pass = toSHA1(pass);
+        AcountDBContext adb = new AcountDBContext();
+        Account a = adb.login(user, pass);
+        HttpSession session = request.getSession();
+        if (a==null) {
+            request.setAttribute("mess", "Enter username and password!");
+            request.getRequestDispatcher("/views/login.jsp").forward(request, response);           
+        }else if (a.isActive()== false) {
+            request.setAttribute("mess", "Account has been banned");
+            request.getRequestDispatcher("/views/login.jsp").forward(request, response);
+        }else{            
+            session.setAttribute("acc", a);
+            response.sendRedirect("home");
+        }        
+   }
+       public static String toSHA1(String str) {
+		String salt = "asjrlkmcoewj@tjle;oxqskjhdjksjf1jurVn";// Làm cho mật khẩu phức tap
+		String result = null;
+		
+		str = str + salt;
+		try {
+			byte[] dataBytes = str.getBytes("UTF-8");
+			MessageDigest md = MessageDigest.getInstance("SHA-1");
+			result = Base64.encodeBase64String(md.digest(dataBytes));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	
+        
+    
+       
+        
+    
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
